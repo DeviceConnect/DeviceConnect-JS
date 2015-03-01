8b15a5628bda482505a1b68c877bc205a9efa2f6
Mitsuhiro Suzuki <mitsuhiro@gclue.jp>
2015-03-01 15:54:53 +0900

    Add humandetect.js

--- dConnectJavascriptApp/js/profile/humandetect.js
+++ dConnectJavascriptApp/js/profile/humandetect.js
@@ -17,12 +17,13 @@ function showHumanDetect(serviceId) {
     initAll();
     setTitle("Human Detect Profile", "black");
 
-    var btnStr = getBackButton('Device Top', 'doCanvasBack', serviceId, "");
+    var btnStr = getBackButton('Device Top', 'doHumanDetectBack', serviceId, "");
     reloadHeader(btnStr);
     reloadFooter(btnStr);
 
     var str = "";
-    str += '<li><a href="javascript:showHumanDetectGet(\'' + serviceId + '\');" value="send">HumanDetect Body Detection GET API</a></li>';
+    str += '<li><a href="javascript:showHumanDetectGet(\'' + serviceId + '\');" value="send">HumanDetect Detection GET API</a></li>';
+    str += '<li><a href="javascript:showHumanDetectEvent(\'' + serviceId + '\');" value="send">HumanDetect Detection EVENT API</a></li>';
     
     reloadList(str);
 }
@@ -58,7 +59,12 @@ function doGetUriFromPath(serviceId, path){
  * @param {String} serviceId サービスID
  * @param {String} sessionKey セッションKEY
  */
-function doCanvasBack(serviceId, sessionKey){
+function doHumanDetectBack(serviceId, sessionKey){
+    if (sessionKey != "") {
+        doHumanDetectBodyUnregister(serviceId, sessionKey);
+        doHumanDetectHandUnregister(serviceId, sessionKey);
+        doHumanDetectFaceUnregister(serviceId, sessionKey);
+    }
     searchDevice(serviceId);
 }
 
@@ -68,7 +74,7 @@ function doCanvasBack(serviceId, sessionKey){
  * @param {String} serviceId サービスID
  * @param {String} sessionKey セッションKEY
  */
-function doHumanDetectBodyGetBack(serviceId, sessionKey){
+function doHumanDetectBodyBack(serviceId, sessionKey){
     showHumanDetect(serviceId);
 }
 
@@ -81,7 +87,7 @@ function showHumanDetectGet(serviceId) {
     initAll();
     setTitle("HumanDetect Profile(GET API)");
 
-    var btnStr = getBackButton('HumanDetect Top', 'doHumanDetectBodyGetBack', serviceId, "");
+    var btnStr = getBackButton('HumanDetect Top', 'doHumanDetectBodyBack', serviceId, "");
     reloadHeader(btnStr);
     reloadFooter(btnStr);
     
@@ -212,4 +218,220 @@ function getHumanDetectResponseString(json) {
     str += 'handDetects=' + (json.handDetects ? json.handDetects.length : 'undefined') + '<br>';
     str += 'faceDetects=' + (json.faceDetects ? json.faceDetects.length : 'undefined') + '<br>';
     return str;
+}
+
+/**
+ * 人体検知Eventフォームを表示する.
+ *
+ * @param {String} serviceId　サービスID
+ */
+function showHumanDetectEvent(serviceId) {
+    initAll();
+    setTitle("Human Detect Profile(Event)");
+
+    var sessionKey = currentClientId;
+    var btnStr = getBackButton('Device Top','doHumanDetectBack', serviceId, sessionKey);
+    reloadHeader(btnStr);
+    reloadFooter(btnStr);
+
+    var str = "";
+    str += '<form  name="humanDetectForm">';
+    str += 'Event(body)<br>';
+    str += '<input type="text" id="eventInfoBody" width="100%">';
+    str += 'Event(hand)<br>';
+    str += '<input type="text" id="eventInfoHand" width="100%">';
+    str += 'Event(face)<br>';
+    str += '<input type="text" id="eventInfoFace" width="100%">';
+    str += '</form>';
+    reloadContent(str);
+
+    doHumanDetectFaceRegist(serviceId, sessionKey);
+    dConnect.connectWebSocket(sessionKey, function(errorCode, errorMessage) {});
+}
+/**
+ * Human Detect Event Register (body)
+ */
+function doHumanDetectBodyeRegist(serviceId, sessionKey) {
+    var builder = new dConnect.URIBuilder();
+    builder.setProfile("humandetect");
+    builder.setAttribute("onbodydetection");
+    builder.setServiceId(serviceId);
+    builder.setAccessToken(accessToken);
+    builder.setSessionKey(sessionKey);
+    var uri = builder.build();
+    if (DEBUG) console.log("Uri: " + uri);
+    
+    dConnect.addEventListener(uri,function(message) {
+        // イベントメッセージが送られてくる
+        if(DEBUG) console.log("Event-Message:"+message)
+        
+        var json = JSON.parse(message);
+        
+        var bodyLength = 0;
+        var handLength = 0;
+        var faceLength = 0;
+        if (json.bodyDetects) {
+            bodyLength = json.bodyDetects.length;
+        }
+        if (json.handDetects) {
+            handLength = json.handDetects.length;
+        }
+        if (json.faceDetects) {
+            faceLength = json.faceDetects.length;
+        }
+        
+        var eventInfo = "[" + getStrTime() + "] body:" + bodyLength + " hand:" + handLength + " face:" + faceLength;
+        $('#eventInfoBody').val(eventInfo);
+        
+    }, null, function(errorCode, errorMessage){
+        alert(errorMessage);
+    });
+}
+
+/**
+ * Human Detect Event Register (hand)
+ */
+function doHumanDetectHandRegist(serviceId, sessionKey) {
+    var builder = new dConnect.URIBuilder();
+    builder.setProfile("humandetect");
+    builder.setAttribute("onhanddetection");
+    builder.setServiceId(serviceId);
+    builder.setAccessToken(accessToken);
+    builder.setSessionKey(sessionKey);
+    var uri = builder.build();
+    if (DEBUG) console.log("Uri: " + uri);
+    
+    dConnect.addEventListener(uri,function(message) {
+        // イベントメッセージが送られてくる
+        if(DEBUG) console.log("Event-Message:"+message)
+        
+        var json = JSON.parse(message);
+        
+        var bodyLength = 0;
+        var handLength = 0;
+        var faceLength = 0;
+        if (json.bodyDetects) {
+            bodyLength = json.bodyDetects.length;
+        }
+        if (json.handDetects) {
+            handLength = json.handDetects.length;
+        }
+        if (json.faceDetects) {
+            faceLength = json.faceDetects.length;
+        }
+        
+        var eventInfo = "[" + getStrTime() + "] body:" + bodyLength + " hand:" + handLength + " face:" + faceLength;
+        $('#eventInfoHand').val(eventInfo);
+        
+    }, null, function(errorCode, errorMessage){
+        alert(errorMessage);
+    });
+}
+
+/**
+ * Human Detect Event Register (face)
+ */
+function doHumanDetectFaceRegist(serviceId, sessionKey) {
+    var builder = new dConnect.URIBuilder();
+    builder.setProfile("humandetect");
+    builder.setAttribute("onfacedetection");
+    builder.setServiceId(serviceId);
+    builder.setAccessToken(accessToken);
+    builder.setSessionKey(sessionKey);
+    var uri = builder.build();
+    if (DEBUG) console.log("Uri: " + uri);
+    
+    dConnect.addEventListener(uri,function(message) {
+        // イベントメッセージが送られてくる
+        if(DEBUG) console.log("Event-Message:"+message)
+        
+        var json = JSON.parse(message);
+        
+        var bodyLength = 0;
+        var handLength = 0;
+        var faceLength = 0;
+        if (json.bodyDetects) {
+            bodyLength = json.bodyDetects.length;
+        }
+        if (json.handDetects) {
+            handLength = json.handDetects.length;
+        }
+        if (json.faceDetects) {
+            faceLength = json.faceDetects.length;
+        }
+        
+        var eventInfo = "[" + getStrTime() + "] body:" + bodyLength + " hand:" + handLength + " face:" + faceLength;
+        $('#eventInfoFace').val(eventInfo);
+        
+    }, null, function(errorCode, errorMessage){
+        alert(errorMessage);
+    });
+}
+
+/**
+ * Human Detect Event Unregister (body)
+ */
+function doHumanDetectBodyUnregister(serviceId, sessionKey) {
+
+    var builder = new dConnect.URIBuilder();
+    builder.setProfile("humandetect");
+    builder.setAttribute("onbodydetection");
+    builder.setServiceId(serviceId);
+    builder.setAccessToken(accessToken);
+    builder.setSessionKey(sessionKey);
+    var uri = builder.build();
+    if (DEBUG) console.log("Uri : "+uri);
+
+    dConnect.removeEventListener(uri, null, function(errorCode, errorMessage){
+        alert(errorMessage);
+    });
+}
+
+/**
+ * Human Detect Event Unregister (hand)
+ */
+function doHumanDetectHandUnregister(serviceId, sessionKey) {
+
+    var builder = new dConnect.URIBuilder();
+    builder.setProfile("humandetect");
+    builder.setAttribute("onhanddetection");
+    builder.setServiceId(serviceId);
+    builder.setAccessToken(accessToken);
+    builder.setSessionKey(sessionKey);
+    var uri = builder.build();
+    if (DEBUG) console.log("Uri : "+uri);
+
+    dConnect.removeEventListener(uri, null, function(errorCode, errorMessage){
+        alert(errorMessage);
+    });
+}
+
+/**
+ * Human Detect Event Unregister (face)
+ */
+function doHumanDetectFaceUnregister(serviceId, sessionKey) {
+
+    var builder = new dConnect.URIBuilder();
+    builder.setProfile("humandetect");
+    builder.setAttribute("onfacedetection");
+    builder.setServiceId(serviceId);
+    builder.setAccessToken(accessToken);
+    builder.setSessionKey(sessionKey);
+    var uri = builder.build();
+    if (DEBUG) console.log("Uri : "+uri);
+
+    dConnect.removeEventListener(uri, null, function(errorCode, errorMessage){
+        alert(errorMessage);
+    });
+}
+
+function getStrTime() {
+	var now = new Date();
+
+	//時・分・秒を取得する
+	var hour = now.getHours();
+	var minute = now.getMinutes();
+	var second = now.getSeconds();
+	var strTime = hour + ":" + minute + ":" + second;
+	return strTime;
 }
