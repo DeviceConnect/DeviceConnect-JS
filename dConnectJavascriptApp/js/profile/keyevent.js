dbd8d335f505dab3badfc9a649b52d8e739b0f21
畠山剛雄 <takeo@gclue.jp>
2015-03-06 19:02:39 +0900

    Add GET method.

--- dConnectJavascriptApp/js/profile/keyevent.js
+++ dConnectJavascriptApp/js/profile/keyevent.js
@@ -18,9 +18,63 @@ function showKeyEvent(serviceId) {
     reloadFooter(btnStr);
 
     var str = "";
+        str += '<li><a href="javascript:showOnDown(\'' + serviceId + '\');" >Show Key(onDown)</a></li>';
+        str += '<li><a href="javascript:showOnUp(\'' + serviceId + '\');" >Show Key(onUp)</a></li>';
         str += '<li><a href="javascript:showDownEvent(\'' + serviceId + '\');" >Key Event(onDown)</a></li>';
         str += '<li><a href="javascript:showUpEvent(\'' + serviceId + '\');" >Key Event(onUp)</a></li>';
     reloadList(str);
+}
+
+/**
+ * Show OnDown
+ *
+ * @param {String}serviceId サービスID
+ */
+function showOnDown(serviceId){
+    initAll();
+    setTitle("Show Down");
+
+    var sessionKey = currentClientId;
+
+    var btnStr = getBackButton('Device Top', 'doKeyBack', serviceId, sessionKey);
+    reloadHeader(btnStr);
+    reloadFooter(btnStr);
+    
+    var str = "";
+    str += '<form name="ondownForm">';
+    str += 'Down<br>';
+    str += '<input type="button" name="getButton" id="getButton" value="Get" onclick="doGetOnDown(\'' + serviceId + '\');"/>';
+    str += '<input type="text" id="idD" width="100%">';
+    str += '<input type="text" id="configD" width="100%">';
+    str += '</form>';
+    reloadContent(str);
+}
+
+/**
+ * Get OnDown
+ *
+ * @param {String}serviceId サービスID
+ */
+function doGetOnDown(serviceId){
+    var builder = new dConnect.URIBuilder();
+    builder.setProfile("keyevent");
+    builder.setAttribute("ondown");
+    builder.setServiceId(serviceId);
+    builder.setAccessToken(accessToken);
+    var uri = builder.build();
+    if (DEBUG) console.log("Uri: " + uri);
+    
+    dConnect.get(uri, null, null, function(json) {
+        if (DEBUG) console.log("Response: ", json);
+        
+        closeLoading();
+        if (json.keyevent) {
+            $('#idD').val("KeyID: "+json.keyevent.id);
+            $('#configD').val("config: "+json.keyevent.config);
+        }
+    }, function(errorCode, errorMessage) {
+        showError("GET ondown", errorCode, errorMessage);
+    });
 }
 
 /**
@@ -48,6 +102,58 @@ function showDownEvent(serviceId){
 
     doDownRegist(serviceId, sessionKey);
     dConnect.connectWebSocket(sessionKey, function(errorCode, errorMessage) {});
+}
+
+/**
+ * Show OnUp
+ *
+ * @param {String}serviceId サービスID
+ */
+function showOnUp(serviceId){
+    initAll();
+    setTitle("Show Up");
+
+    var sessionKey = currentClientId;
+
+    var btnStr = getBackButton('Device Top', 'doKeyBack', serviceId, sessionKey);
+    reloadHeader(btnStr);
+    reloadFooter(btnStr);
+    
+    var str = "";
+    str += '<form name="onupForm">';
+    str += 'Up<br>';
+    str += '<input type="button" name="getButton" id="getButton" value="Get" onclick="doGetOnUp(\'' + serviceId + '\');"/>';
+    str += '<input type="text" id="idU" width="100%">';
+    str += '<input type="text" id="configU" width="100%">';
+    str += '</form>';
+    reloadContent(str);
+}
+
+/**
+ * Get OnUp
+ *
+ * @param {String}serviceId サービスID
+ */
+function doGetOnUp(serviceId){
+    var builder = new dConnect.URIBuilder();
+    builder.setProfile("keyevent");
+    builder.setAttribute("onup");
+    builder.setServiceId(serviceId);
+    builder.setAccessToken(accessToken);
+    var uri = builder.build();
+    if (DEBUG) console.log("Uri: " + uri);
+    
+    dConnect.get(uri, null, null, function(json) {
+        if (DEBUG) console.log("Response: ", json);
+        
+        closeLoading();
+        if (json.keyevent) {
+            $('#idU').val("KeyID: "+json.keyevent.id);
+            $('#configU').val("config: "+json.keyevent.config);
+        }
+    }, function(errorCode, errorMessage) {
+        showError("GET ondown", errorCode, errorMessage);
+    });
 }
 
 /**
@@ -85,6 +191,16 @@ function showUpEvent(serviceId){
  */
 function doKeyEventBack(serviceId, sessionKey){
     searchSystem(serviceId);
+}
+
+/**
+ * Backボタン
+ *
+ * serviceId サービスID
+ * sessionKey セッションKEY
+ */
+function doKeyBack(serviceId, sessionKey){
+    showKeyEvent(serviceId);
 }
 
 /**
