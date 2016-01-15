5aa4567ca25d740902a6f2ef251a16a3e0b330ca
hayato-ito <hayato@gclue.jp>
2016-01-15 19:16:59 +0900

    Modified proximity profile.

--- dConnectJavascriptApp/js/profile/proximity.js
+++ dConnectJavascriptApp/js/profile/proximity.js
@@ -1,6 +1,6 @@
 /**
  proximity.js
- Copyright (c) 2014 NTT DOCOMO,INC.
+ Copyright (c) 2016 NTT DOCOMO,INC.
  Released under the MIT license
  http://opensource.org/licenses/mit-license.php
  */
@@ -8,74 +8,72 @@
 /**
  * Proximity Profile
  *
- * @param {String} serviceId サービスID
+ * @param {String} serviceId service ID
  */
 function showProximity(serviceId) {
-
   initAll();
 
-  var btnStr = '';
-  btnStr += getBackButton('Device Top', 'doProximityBack', serviceId, '');
+  setTitle('Proximity Profile');
+  var btnStr = getBackButton('Device Top', 'doProximityBack', serviceId, '');
   reloadHeader(btnStr);
   reloadFooter(btnStr);
 
-  setTitle('Proximity Profile');
-
   var str = '';
-  if (myDeviceName.indexOf('Host') != -1) {
-    str += '<li><a href="javascript:showUserProximity(\'' +
-          serviceId + '\');" value="Setting">user proximity</a></li>';
-  } else if (myDeviceName.indexOf('Dice+') != -1) {
-    str += '<li><a href="javascript:showDeviceProximity(\'' +
-          serviceId + '\');" value="Call">device proximity</a></li>';
-  }
+  str += '<li><a href="javascript:showUserProximity(\'' + serviceId +
+         '\');" value="Setting">user proximity</a></li>';
+
+  str += '<li><a href="javascript:showDeviceProximity(\'' + serviceId +
+         '\');" value="Call">device proximity</a></li>';
 
   reloadList(str);
 }
 
 /**
- * Device Proximityの処理
+ * Show Device Proximity
  *
- * @param {String}serviceId サービスID
+ * @param {String}serviceId service ID
  */
 function showDeviceProximity(serviceId) {
   initAll();
-  var sessionKey = currentClientId;
+  setTitle('Device Proximity');
 
-  var btnStr = '';
-  btnStr += getBackButton('Proximity Top', 'doDeviceProximityBack',
+  var sessionKey = currentClientId;
+  var btnStr = getBackButton('Proximity Top', 'doDeviceProximityBack',
                     serviceId, sessionKey);
   reloadHeader(btnStr);
   reloadFooter(btnStr);
 
-  setTitle('Device Proximity');
-
   var str = '';
   str += '<div>';
-  str += '  <input data-icon=\"search\" onclick=\"doGetDeviceProximity(\'' +
-        serviceId + '\')\" type=\"button\" value=\"Get\" />';
+
+  str += '<input data-icon="search" onclick="doGetDeviceProximity(\'' +
+         serviceId + '\')" type="button" value="Get" />';
   str += '</div>';
-  str += '<fieldset class=\"ui-grid-a\">';
-  str += '  <div class=\"ui-block-a\">';
-  str += '    <input data-icon=\"search\"' +
-        ' onclick=\"doRegisterDeviceProximity(\'' +
-        serviceId + '\', \'' +  sessionKey +
-        '\')\" type=\"button\" value=\"Register\" />';
-  str += '  </div>';
-  str += '  <div class=\"ui-block-b\">';
-  str += '    <input data-icon=\"search\"' +
-        ' onclick=\"doUnregisterDeviceProximity(\'' +
-        serviceId + '\', \'' +  sessionKey +
-        '\')\" type=\"button\" value=\"Unregister\" />';
-  str += '  </div>';
+
+  str += '<fieldset class="ui-grid-a">';
+  str += '<div class="ui-block-a">';
+  str += '<input data-icon="search" onclick="doRegisterDeviceProximity(\'' +
+          serviceId + '\', \'' + sessionKey + '\')" type="button" value="Register" />';
+  str += '</div>';
+  str += '<div class="ui-block-b">';
+  str += '<input data-icon="search" onclick="doUnregisterDeviceProximity(\'' +
+          serviceId + '\', \'' +  sessionKey + '\')" type="button" value="Unregister" />';
+  str += '</div>';
   str += '</fieldset>';
 
-  str += 'device proximity<br>';
+  str += '<strong>device proximity</strong><br>';
+
   str += '<form  name="deviceProximityForm">';
-  str += '<input type="text" id="value" width="100%">';
-  str += '<input type="text" id="min" width="100%">';
-  str += '<input type="text" id="max" width="100%">';
-  str += '<input type="text" id="threshold" width="100%">';
+  str += '<label>value</label>';
+  str += '<input type="text" id="value" />';
+  str += '<label>min</label>';
+  str += '<input type="text" id="min" />';
+  str += '<label>max</label>';
+  str += '<input type="text" id="max" />';
+  str += '<label>threshold</label>';
+  str += '<input type="text" id="threshold" />';
+  str += '<label>range</label>';
+  str += '<input type="text" id="range" />';
   str += '</form>';
   str += '<hr>';
 
@@ -83,64 +81,59 @@ function showDeviceProximity(serviceId) {
 }
 
 /**
- * User Proximityの表示
+ * Show User Proximity
  *
- * @param {String}serviceId サービスID
+ * @param {String}serviceId service ID
  */
 function showUserProximity(serviceId) {
   initAll();
+  setTitle('User Proximity');
 
   var sessionKey = currentClientId;
-
-  var btnStr = '';
-  btnStr += getBackButton('Proximity Top', 'doUserProximityBack',
+  var btnStr = getBackButton('Proximity Top', 'doUserProximityBack',
                       serviceId, sessionKey);
   reloadHeader(btnStr);
   reloadFooter(btnStr);
 
-  setTitle('User Proximity');
-
   var str = '';
   str += '<div>';
-  str += '  <input data-icon=\"search\" onclick=\"doGetUserProximity(\'' +
-          serviceId + '\')\" type=\"button\" value=\"Get\" />';
+  str += '<input data-icon="search" onclick="doGetUserProximity(\'' +
+          serviceId + '\')" type="button" value="Get" />';
   str += '</div>';
-  str += '<fieldset class=\"ui-grid-a\">';
-  str += '  <div class=\"ui-block-a\">';
-  str += '    <input data-icon=\"search\" ' +
-        'onclick=\"doRegisterUserProximity(\'' +
-        serviceId + '\', \'' +  sessionKey +
-        '\')\" type=\"button\" value=\"Register\" />';
-  str += '  </div>';
-  str += '  <div class=\"ui-block-b\">';
-  str += '    <input data-icon=\"search\"' +
-        ' onclick=\"doUnregisterUserProximity(\'' +  serviceId + '\', \'' +
-        sessionKey + '\')\" type=\"button\" value=\"Unregister\" />';
-  str += '  </div>';
+  str += '<fieldset class="ui-grid-a">';
+  str += '<div class="ui-block-a">';
+  str += '<input data-icon="search" onclick="doRegisterUserProximity(\'' +
+          serviceId + '\', \'' +  sessionKey + '\')" type="button" value="Register" />';
+  str += '</div>';
+  str += '<div class="ui-block-b">';
+  str += '<input data-icon="search" onclick="doUnregisterUserProximity(\'' +
+          serviceId + '\', \'' + sessionKey + '\')" type="button" value="Unregister" />';
+  str += '</div>';
   str += '</fieldset>';
 
   str += 'user proximity<br>';
   str += '<form  name="userProximityForm">';
   str += '<input type="text" id="user" width="100%">';
   str += '</form>';
+
   reloadContent(str);
 }
 
 /**
- * Menu Backボタン
+ * Menu Back button
  *
- * serviceId {String} サービスID
- * sessionKey {String} セッションKEY
+ * serviceId {String} service ID
+ * sessionKey {String} session Key
  */
 function doProximityBack(serviceId, sessionKey) {
   searchSystem(serviceId);
 }
 
 /**
- * User Proximity Backボタン
+ * User Proximity Back Button
  *
- * serviceId {String} サービスID
- * sessionKey {String} セッションKEY
+ * serviceId {String} service ID
+ * sessionKey {String} session Key
  */
 function doUserProximityBack(serviceId, sessionKey) {
   showProximity(serviceId);
@@ -148,10 +141,10 @@ function doUserProximityBack(serviceId, sessionKey) {
 }
 
 /**
- * User Proximity Backボタン
+ * User Proximity Back Button
  *
- * serviceId {String} サービスID
- * sessionKey {String} セッションKEY
+ * serviceId {String} service ID
+ * sessionKey {String} session Key
  */
 function doDeviceProximityBack(serviceId, sessionKey) {
   showProximity(serviceId);
@@ -159,8 +152,8 @@ function doDeviceProximityBack(serviceId, sessionKey) {
 }
 
 /**
- * Proximity の取得
- * @param {String} serviceId サービスID
+ * Get Proximity
+ * @param {String} serviceId service ID
  */
 function doGetDeviceProximity(serviceId) {
   var builder = new dConnect.URIBuilder();
@@ -169,16 +162,16 @@ function doGetDeviceProximity(serviceId) {
   builder.setServiceId(serviceId);
   builder.setAccessToken(accessToken);
   var uri = builder.build();
-  if (DEBUG) {
-    console.log('Uri:' +  uri)
-  }
+  if (DEBUG) { console.log('Uri:' +  uri); }
   dConnect.get(uri, null, function(json) {
+    if (DEBUG) { console.log('result:' +  json); }
     alert(JSON.stringify(json));
     if (json.proximity) {
       $('#value').val(json.proximity.value);
       $('#min').val(json.proximity.min);
       $('#max').val(json.proximity.max);
       $('#threshold').val(json.proximity.threshold);
+      $('#range').val(json.proximity.range);
     }
   }, function(errorCode, errorMessage) {
     alert('errorCode=' +  errorCode + ' errorMessage=' +  errorMessage);
@@ -186,10 +179,10 @@ function doGetDeviceProximity(serviceId) {
 }
 
 /**
- * Device Proximityイベントの登録
+ * Register Device Proximity Event
  *
- * @param {String} serviceId {String} サービスID
- * @param {String} sessionKey {String} セッションキー
+ * @param {String} serviceId {String} service ID
+ * @param {String} sessionKey {String} session Key
  */
 function doRegisterDeviceProximity(serviceId, sessionKey) {
   var builder = new dConnect.URIBuilder();
@@ -199,35 +192,29 @@ function doRegisterDeviceProximity(serviceId, sessionKey) {
   builder.setAccessToken(accessToken);
   builder.addParameter('sessionKey', sessionKey);
   var uri = builder.build();
-  if (DEBUG) {
-    console.log('Uri:' +  uri);
-  }
+  if (DEBUG) { console.log('Uri:' +  uri); }
   dConnect.addEventListener(uri, function(message) {
-    // イベントメッセージが送られてくる
-    if (DEBUG) {
-      console.log('Event-Message:' +  message);
-    }
+    if (DEBUG) { console.log('Event-Message:' +  message); }
     var json = JSON.parse(message);
     if (json.proximity) {
       $('#value').val(json.proximity.value);
       $('#min').val(json.proximity.min);
       $('#max').val(json.proximity.max);
       $('#threshold').val(json.proximity.threshold);
+      $('#range').val(json.proximity.range);
     }
   }, function() {
-    if (DEBUG) {
-      console.log('Success to register proximity');
-    }
+    if (DEBUG) { console.log('Success to register proximity'); }
   }, function(errorCode, errorMessage) {
     alert('errorCode=' +  errorCode + ' errorMessage=' +  errorMessage);
   });
 }
 
 /**
- * Device Proximityイベントの削除
+ * Delete Device Proximity Event
  *
- * @param {String} serviceId サービスID
- * @param {String} sessionKey セッションキー
+ * @param {String} serviceId service ID
+ * @param {String} sessionKey session Key
  */
 function doUnregisterDeviceProximity(serviceId, sessionKey) {
   var builder = new dConnect.URIBuilder();
@@ -237,13 +224,9 @@ function doUnregisterDeviceProximity(serviceId, sessionKey) {
   builder.setAccessToken(accessToken);
   builder.addParameter('sessionKey', sessionKey);
   var uri = builder.build();
-  if (DEBUG) {
-    console.log('Uri:' +  uri);
-  }
+  if (DEBUG) { console.log('Uri:' +  uri); }
   dConnect.removeEventListener(uri, function() {
-    if (DEBUG) {
-      console.log('Success to unregister proximity');
-    }
+    if (DEBUG) { console.log('Success to unregister proximity'); }
   }, function(errorCode, errorMessage) {
     alert('errorCode=' +  errorCode + ' errorMessage=' +  errorMessage);
   });
@@ -256,10 +239,9 @@ function doGetUserProximity(serviceId) {
   builder.setServiceId(serviceId);
   builder.setAccessToken(accessToken);
   var uri = builder.build();
-  if (DEBUG) {
-    console.log('Uri:' +  uri);
-  }
+  if (DEBUG) { console.log('Uri:' +  uri); }
   dConnect.get(uri, null, function(json) {
+    if (DEBUG) { console.log('Response:' +  json); }
     alert(JSON.stringify(json));
     if (json.proximity) {
       $('#user').val(json.proximity.near);
@@ -270,10 +252,10 @@ function doGetUserProximity(serviceId) {
 }
 
 /**
- * User Proximityイベントの登録
+ * Register User Proximity Event
  *
- * @param {String} serviceId サービスID
- * @param {String} sessionKey セッションキー
+ * @param {String} serviceId service ID
+ * @param {String} sessionKey session Key
  */
 function doRegisterUserProximity(serviceId, sessionKey) {
   var builder = new dConnect.URIBuilder();
@@ -283,32 +265,25 @@ function doRegisterUserProximity(serviceId, sessionKey) {
   builder.setAccessToken(accessToken);
   builder.addParameter('sessionKey', sessionKey);
   var uri = builder.build();
-  if (DEBUG) {
-    console.log('Uri:' +  uri);
-  }
+  if (DEBUG) { console.log('Uri:' +  uri); }
   dConnect.addEventListener(uri, function(message) {
-    // イベントメッセージが送られてくる
-    if (DEBUG) {
-      console.log('Response:' +  message);
-    }
+    if (DEBUG) { console.log('Event-Message:' +  message); }
     var json = JSON.parse(message);
     if (json.proximity) {
       $('#user').val(json.proximity.near);
     }
   }, function() {
-    if (DEBUG) {
-      console.log('Success to register proximity');
-    }
+    if (DEBUG) { console.log('Success to register proximity'); }
   }, function(errorCode, errorMessage) {
-    alert(errorMessage);
+    alert('errorCode=' +  errorCode + ' errorMessage=' +  errorMessage);
   });
 }
 
 /**
- * User Proximityイベントの削除
+ * Delete User Proximity Event
  *
- * @param {String} serviceId サービスID
- * @param {String} sessionKey セッションキー
+ * @param {String} serviceId service ID
+ * @param {String} sessionKey session Key
  */
 function doUnregisterUserProximity(serviceId, sessionKey) {
   var builder = new dConnect.URIBuilder();
@@ -318,14 +293,10 @@ function doUnregisterUserProximity(serviceId, sessionKey) {
   builder.setAccessToken(accessToken);
   builder.addParameter('sessionKey', sessionKey);
   var uri = builder.build();
-  if (DEBUG) {
-    console.log('Uri:' +  uri);
-  }
+  if (DEBUG) { console.log('Uri:' +  uri); }
   dConnect.removeEventListener(uri, function() {
-    if (DEBUG) {
-      console.log('Success to unregister proximity');
-    }
+    if (DEBUG) { console.log('Success to unregister proximity'); }
   }, function(errorCode, errorMessage) {
-    alert(errorMessage);
+    alert('errorCode=' +  errorCode + ' errorMessage=' +  errorMessage);
   });
 }
