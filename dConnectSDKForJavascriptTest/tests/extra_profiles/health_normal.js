689403def5e88a4df1cee5a1d80915e6d58412a8
TakayukiHoshi1984 <hoshi@gclue.jp>
2017-02-11 10:26:45 +0900

    Healthに関するUIとQUnitテストを追加。

--- dConnectSDKForJavascriptTest/tests/extra_profiles/health_normal.js
+++ dConnectSDKForJavascriptTest/tests/extra_profiles/health_normal.js
@@ -79,3 +79,74 @@ HealthProfileNormalTest.heartEventNormalTest001 = function (assert) {
     });
 }
 QUnit.asyncTest("heartEventNormalTest001", HealthProfileNormalTest.heartEventNormalTest001);
+
+
+
+/**
+ * 心拍数を取得するテストを行う。
+ * <h3>【HTTP通信】</h3>
+ * <p id="test">
+ * Method: GET<br/>
+ * Path: /health/onHeart?serviceId=xxxx&accessToken=xxx<br/>
+ * </p>
+ * <h3>【期待する動作】</h3>
+ * <p id="expected">
+ * ・resultに0が返ってくること。<br/>
+ * ・heartの値が返ってくること。<br/>
+ * </p>
+ */
+HealthProfileNormalTest.onheartNormalTest = function (assert) {
+    searchTestService(function (accessToken, serviceId) {
+        var builder = new dConnect.URIBuilder();
+        builder.setProfile("health");
+        builder.setAttribute("onHeart");
+        builder.setServiceId(serviceId);
+        builder.setAccessToken(accessToken);
+        var uri = builder.build();
+        dConnect.get(uri, null,
+            function (json) {
+                assert.ok(true, "result=" + json.result);
+                assert.ok((json.heart != undefined && json.heart.rate.value >= 0), "heart=" + json.heart);
+                QUnit.start();
+            },
+            function (errorCode, errorMessage) {
+                assert.ok(checkErrorCode(errorCode),
+                    'errorCode=' + errorCode + ' errorMessage=' + errorMessage);
+                QUnit.start();
+            });
+    },
+    function (errorCode, errorMessage) {
+        assert.ok(false, "errorCode=" + errorCode + ", errorMessage=" + errorMessage);
+        QUnit.start();
+    });
+}
+QUnit.asyncTest("onHeart", HealthProfileNormalTest.onheartNormalTest);
+
+/**
+ * Healthプロファイルのheartrateの登録と解除を行うテストを行う。
+ * <h3>【HTTP通信】</h3>
+ * <p id="test">
+ * Method: PUT<br/>
+ * Path: /health/onHeart?serviceId=xxxx&accessToken=xxx<br/>
+ * </p>
+ * <h3>【期待する動作】</h3>
+ * <p id="expected">
+ * ・resultに0が返ってくること。<br/>
+ * ・イベント通知が送られてくること。<br/>
+ * </p>
+ */
+HealthProfileNormalTest.onheartEventNormalTest001 = function (assert) {
+    var builder = new dConnect.URIBuilder();
+    builder.setProfile("health");
+    builder.setAttribute("onHeart");
+    openWebsocket(builder, assert, 10000, function (message) {
+        var json = JSON.parse(message);
+        if (json.profile === "health" && json.attribute === "onHeart") {
+            assert.ok(true, message);
+            assert.ok((json.heart != undefined && json.heart.rate.value >= 0), "heart=" + json.heart);
+            return true;
+        }
+        return false;
+    });
+}
+QUnit.asyncTest("onheartEventNormalTest001", HealthProfileNormalTest.onheartEventNormalTest001);
