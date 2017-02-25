3c942924cb1325e33fdb25d195ccd19a6a9d0be9
TakayukiHoshi1984 <hoshi@gclue.jp>
2017-02-25 12:19:40 +0900

    Get Temperatureの仕様変更によるUIの修正。

--- dConnectJavascriptApp/js/profile/temperature.js
+++ dConnectJavascriptApp/js/profile/temperature.js
@@ -21,19 +21,17 @@ function showTemperature(serviceId) {
 
   var str = '';
   str += '<li><a href="javascript:showGetTemperature(\'' +
-          serviceId + '\');">Get Temperature</a></li>';
+          serviceId + '\',\'\');">Get Temperature</a></li>';
   str += '<li><a href="javascript:showSetTemperature(\'' +
           serviceId + '\');">Set Temperature</a></li>';
   reloadList(str);
 }
-
-
 /**
- * Get Temperature.
+ * Show Get Temperature.
  *
  * @param {String} serviceId サービスID
  */
-function showGetTemperature(serviceId) {
+function showGetTemperature(serviceId, html) {
 
   initAll();
 
@@ -43,11 +41,35 @@ function showGetTemperature(serviceId) {
   btnStr += getBackButton('Temperature Top', 'doTemperatureMenuBack', serviceId, '');
   reloadHeader(btnStr);
   reloadFooter(btnStr);
+  btnStr += '<h2>Temperature Info</h2>';
+  btnStr += html
+  btnStr += '<div data-role="fieldcontain">';
+  btnStr += '  <label for="Type">Type:</label>';
+  btnStr += '  <select id="type">';
+  btnStr += '    <option value="1">摂氏</option>';
+  btnStr += '    <option value="2">華氏</option>';
+  btnStr += '  </select>';
+  btnStr += '</div>';
+  btnStr += '<input data-role="button" type="button" name="button"' +
+        ' id="button" value="Get Temperature"' +
+        ' onclick="javascript:getTemperature(\'' +
+        serviceId + '\');"/><br>';
+  reloadContent(btnStr);
+}
+/**
+ * Get Temperature.
+ *
+ * @param {String} serviceId サービスID
+ */
+function getTemperature(serviceId) {
+  var type = $('#type').val();
 
   var builder = new dConnect.URIBuilder();
   builder.setProfile('temperature');
   builder.setServiceId(serviceId);
   builder.setAccessToken(accessToken);
+  builder.addParameter("type", type);
+
   var uri = builder.build();
 
   if (DEBUG) {
@@ -68,7 +90,7 @@ function showGetTemperature(serviceId) {
 
     var str = '';
     str += '<center><h1>' + temp + '<h1></center>';
-    reloadContent(str);
+    showGetTemperature(serviceId, str);
   }, function(errorCode, errorMessage) {
     closeLoading();
     showError('GET temperature', errorCode, errorMessage);
@@ -76,7 +98,7 @@ function showGetTemperature(serviceId) {
 }
 
 /**
- * Set Temperature.
+ * Show Set Temperature.
  *
  * @param {String} serviceId サービスID
  */
@@ -92,7 +114,7 @@ function showSetTemperature(serviceId) {
   reloadFooter(btnStr);
   btnStr += '<h2>Temperature Info</h2>';
   btnStr += makeInputText('Temperature', 'temperature', 'temperature');
-    btnStr += '<div data-role="fieldcontain">';
+  btnStr += '<div data-role="fieldcontain">';
   btnStr += '  <label for="Type">Type:</label>';
   btnStr += '  <select id="type">';
   btnStr += '    <option value="1">摂氏</option>';
@@ -113,7 +135,6 @@ function showSetTemperature(serviceId) {
 function setTemperature(serviceId) {
   var temp = $('#temperature').val();
   var type = $('#type').val();
-  console.log(temp + " " + type);
   var builder = new dConnect.URIBuilder();
   builder.setProfile('temperature');
   builder.setServiceId(serviceId);
